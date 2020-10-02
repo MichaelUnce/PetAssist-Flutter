59a9a9c829a721c266645e1f280839bc5c9e6ed5
Faith Loredo <Faithc.2555@gmail.com>
2020-10-02 01:09:45 -0400

    Created Scheduller

---
+++ lib/Scheduluer/Schedule.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Schedule extends StatelessWidget {
+  List<Widget> pageChildren(double width) {
+    return <Widget>[
+      
+    ];
+  }
+
+    @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: pageChildren(constraints.biggest.width / 2),
+          );
+        } else {
+          return Column(
+            children: pageChildren(constraints.biggest.width),
+          );
+        }
+      },
+    );
+  }
+}
+
+}
+
